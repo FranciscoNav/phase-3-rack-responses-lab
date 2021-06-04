817bbc6c85004ce69f8da709ff35f10e127b000c
Francisco <fnaveira11@gmail.com>
2021-06-04 18:03:20 -0400

    commit tree e904ca28c6e16afbc4c8c6fc6ad544f131bfd9cd

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,6 @@ require 'pry'
 class Application
 
     def call(env)
-
         resp = Rack::Response.new
 
         current_time = Time.now
@@ -13,8 +12,7 @@ class Application
         else
             resp.write "Good Afternoon!"
         end
+        resp.finish
     end
-
-
 
 end
